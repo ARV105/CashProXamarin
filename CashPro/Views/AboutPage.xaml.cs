e522800d5ea254322b7530b973f69425a966e3e5
ARV105 <aamadeus2424@gmail.com>
2019-01-24 19:35:36 -0500

    Xamarin Example Project for CashPro. First Commit before any major changes.

---
+++ CashPro/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CashPro.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
