fb3298c10bb53c66cb81e5a919ec82c05e84ea98
ARV105 <aamadeus2424@gmail.com>
2019-01-30 21:00:32 -0500

    Added Tabbed Page cs and xml

---
+++ CashPro/Views/TabbPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CashPro.Views
+{
+    public partial class TabbPage : ContentPage
+    {
+        public TabbPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
